d7d785c251076707c08df76824271e4bfdb946cc
Tim Quelch <tim@quelch.name>
2017-11-10 13:12:48 +1000

    Add timing ouput to main

--- src/mazes.cpp
+++ src/mazes.cpp
@@ -1,15 +1,43 @@
+#include <chrono>
+#include <iostream>
+
 #include "maze.h"
 #include "solvers.h"
 
+using hr_clock = std::chrono::high_resolution_clock;
+
 int main() {
-    mazes::Maze maze(31, 0.15);
-    maze.print();
-    maze.printGraph();
-    maze.writePng("maze.png");
-    maze.writePngGraph("graph.png");
+    std::cout << "Generating maze... " << std::flush;
+    auto start = hr_clock::now();
+    mazes::Maze maze(201, 0.10);
+    auto d = std::chrono::duration_cast<std::chrono::milliseconds>(hr_clock::now() - start);
+    std::cout << "Time elapsed = " << d.count() << " ms\n";
+
+    // maze.print();
+    // maze.printGraph();
+    // maze.writePng("maze.png");
+    // maze.writePngGraph("graph.png");
+
+    std::cout << "Solving using BFS... " << std::flush;
+    start = hr_clock::now();
     auto bfs = solveBfs(maze, maze.getStartNode(), maze.getEndNode());
+    d = std::chrono::duration_cast<std::chrono::milliseconds>(hr_clock::now() - start);
+    std::cout << "Time elapsed = " << d.count() << " ms\n";
+
+    std::cout << "Solving using DFS... " << std::flush;
+    start = hr_clock::now();
     auto dfs = solveDfs(maze, maze.getStartNode(), maze.getEndNode());
+    d = std::chrono::duration_cast<std::chrono::milliseconds>(hr_clock::now() - start);
+    std::cout << "Time elapsed = " << d.count() << " ms\n";
+
+    std::cout << "Solving using Dijkstra... " << std::flush;
+    start = hr_clock::now();
+    auto dij = solveDijkstra(maze, maze.getStartNode(), maze.getEndNode());
+    d = std::chrono::duration_cast<std::chrono::milliseconds>(hr_clock::now() - start);
+    std::cout << "Time elapsed = " << d.count() << " ms\n";
+
     maze.writePngPath(bfs, "path-bfs.png");
     maze.writePngPath(dfs, "path-dfs.png");
+    maze.writePngPath(dij, "path-dij.png");
     return 0;
 }
